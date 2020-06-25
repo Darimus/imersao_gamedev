d6c7440bdd11ab62a752318943489f7ec2f9921e
Darimus <bruno.passos@outlook.com.br>
2020-06-25 15:53:28 -0300

    first commit

---
+++ scripts/jogo/inimigo.js
@@ -0,0 +1,14 @@
+class Inimigo extends Animacao {
+    constructor(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite) {
+        super(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite)
+
+        this.velocidade = 35;
+    }
+    move(){
+        this.x = this.x - this.velocidade;
+
+        if(this.x < -this.largura){
+            this.x = width;
+        }
+    }
+}
---
+++ scripts/jogo/personagem.js
@@ -0,0 +1,27 @@
+class Personagem extends Animacao{
+    constructor(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite) {
+        super(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite) 
+        
+        this.yInicial = height - this.altura;
+        this.y = this.yInicial;
+        this.velocidadeDoPulo = 0;
+        this.gravidade = 3;
+    }
+    pula(){
+        this.velocidadeDoPulo = -35;
+    }
+
+    aplicaGravidade() {
+        this.y = this.y + this.velocidadeDoPulo;
+        this.velocidadeDoPulo = this.velocidadeDoPulo + this.gravidade;
+
+        if(this.y > this.yInicial){
+            this.y = this.yInicial;
+        }
+    }
+    estaColidindo(inimigo) {
+        const precisao = .7;
+        const colisao = collideRectRect(this.x, this.y, this.largura * .7, this.altura * .7, inimigo.x, inimigo.y, inimigo.largura * .7, inimigo.altura * .7);
+        return colisao;
+    }
+}
